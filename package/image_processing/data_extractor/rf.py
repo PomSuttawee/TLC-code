243d3e26761caab811ef3f72ca3fe460decb7c3f
Suttawee Thongkao <112872479+PomSuttawee@users.noreply.github.com>
2025-03-01 10:03:28 +0700

    Add functions to detect centroids and calculate RF values; update image processing methods to include centroid visualization

--- package/image_processing/data_extractor/rf.py
+++ package/image_processing/data_extractor/rf.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
+from package.image_processing.segmentation import util
 
 def _calculate_vertical_intensity(image: np.ndarray) -> np.ndarray:
     """
@@ -15,6 +16,36 @@ def _calculate_vertical_intensity(image: np.ndarray) -> np.ndarray:
         np.ndarray: Average intensity of the image vertically.
     """
     return np.mean(image, axis=1)
+
+def _remove_contour(list_contour: list, min_area: int) -> list:
+    return [contour for contour in list_contour if cv2.contourArea(contour) > min_area]
+
+def _get_contour(image: np.ndarray, min_area: int) -> list:
+    mode = cv2.RETR_EXTERNAL
+    method = cv2.CHAIN_APPROX_NONE
+    list_contour, _ = cv2.findContours(image, mode, method)
+    if min_area < 0:
+        return list_contour
+    return _remove_contour(list_contour, min_area)
+
+def _get_bounding_box(list_contour: list) -> list:
+        list_box = [(x, y, w, h) for contour in list_contour for x, y, w, h in [cv2.boundingRect(contour)]]
+        return sorted(util.groupBoundingBox(list_box), key=lambda x: x[0])
+
+def _detect_centroids(image: np.ndarray) -> List[int]:
+    """
+    Detect the centroids of the image.
+
+    Args:
+        image (np.ndarray): Input image.
+
+    Returns:
+        List[int]: Centroids of the image.
+    """
+    binary_image = np.where(image > 0, 255, 0).astype(np.uint8)
+    contours = _get_contour(binary_image, 200)
+    bounding_box_list = _get_bounding_box(contours)
+    return sorted([y + h // 2 for x, y, w, h in bounding_box_list])
 
 def _find_peaks_in_intensity(average_intensity: np.ndarray) -> Tuple[np.ndarray, dict]:
     """
@@ -29,7 +60,7 @@ def _find_peaks_in_intensity(average_intensity: np.ndarray) -> Tuple[np.ndarray,
     average_intensity = average_intensity.flatten()
     return find_peaks(average_intensity, prominence=10, distance=20)
 
-def calculate_rf(image: np.ndarray) -> List[float]:
+def calculate_rf_detect_peak(image: np.ndarray) -> List[float]:
     """
     Calculate the RF value of the image.
 
@@ -43,32 +74,43 @@ def calculate_rf(image: np.ndarray) -> List[float]:
     peaks = _find_peaks_in_intensity(vertical_intensity)[0]
     return [round((image.shape[0] - peak) / image.shape[0], 2) for peak in peaks]
 
-def plot_vertical_intensity(image: np.ndarray):
+def calculate_rf_detect_centroid(image: np.ndarray) -> List[float]:
     """
-    Plot the average intensity of the image vertically.
+    Calculate the RF value of the image.
 
     Args:
         image (np.ndarray): Input image.
+
+    Returns:
+        List[float]: RF value of the image.
     """
-    vertical_intensity = _calculate_vertical_intensity(image)
-    data = vertical_intensity[:, 2]
-    peaks, properties = find_peaks(data)
-    image_with_peak_lines = image.copy()
-    
-    plt.figure(figsize=(20, 5))
-    plt.subplot(1, 2, 1)
-    plt.plot(data)
-    for peak in peaks:
-        plt.axvline(x=peak, color='r', linestyle='--')
-        image_with_peak_lines = cv2.line(image_with_peak_lines, (0, peak), (image.shape[1], peak), (0, 0, 255), 2)
-    plt.vlines(x=peaks, ymin=data[peaks] - properties["prominences"], ymax=data[peaks], color="C1", linestyles="dashed")
-    plt.hlines(y=properties["width_heights"], xmin=properties["left_ips"], xmax=properties["right_ips"], color="C1", linestyles="dashed")
-    plt.title("Average Vertical Intensity")
-    plt.xlabel("Row")
-    plt.ylabel("Intensity")
-    
-    plt.subplot(1, 2, 2)
-    plt.imshow(cv2.cvtColor(image_with_peak_lines, cv2.COLOR_BGR2RGB))
-    plt.title("Image")
-    plt.axis('off')
-    plt.show()
+    centroids = _detect_centroids(image)
+    return [round((image.shape[0] - centroid) / image.shape[0], 2) for centroid in centroids]
+
+def get_image_with_centroids(image: np.ndarray) -> np.ndarray:
+    """
+    Get the image with centroids.
+
+    Args:
+        image (np.ndarray): Input image.
+
+    Returns:
+        np.ndarray: Image with centroids.
+    """
+    centroids = _detect_centroids(image)
+    rf = calculate_rf_detect_centroid(image)
+    image_with_centroid = image.copy()
+    for rf_value, centroid in zip(rf, centroids):
+        cv2.circle(img = image_with_centroid,
+                   center = (image.shape[1] // 2, centroid), 
+                   radius = 4, 
+                   color = (255),
+                   thickness = -1)
+        cv2.putText(img = image_with_centroid,
+                    text = str(rf_value),
+                    org = (image.shape[1] // 2 - 20, centroid - 8),
+                    fontFace = cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale = 0.6,
+                    color = (255),
+                    thickness = 2)
+    return image_with_centroid
