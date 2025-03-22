52d81f4ae393d059f9fbd86bf5f581f20f92b021
Suttawee Thongkao <112872479+PomSuttawee@users.noreply.github.com>
2025-03-22 21:45:59 +0700

    Enhance substance spot cropping by adding final bounding box creation and visualization improvements

--- package_2/image_processing/crop_substance_spot.py
+++ package_2/image_processing/crop_substance_spot.py
@@ -25,14 +25,19 @@ def crop_substance_spot(image):
     checked_overlapping_boxes = check_overlap(image, highest_concentration_bounding_boxes)
     assert checked_overlapping_boxes, "No checked overlapping boxes found"
     
+    final_bounding_boxes = create_bounding_boxes_from_checked_boxes(bounding_boxes, checked_overlapping_boxes)
+    assert final_bounding_boxes, "No final bounding boxes found"
     
+    final_mask = create_mask_from_bounding_boxes(image.shape, final_bounding_boxes)
+    assert np.sum(final_mask) > 0, "Final mask is empty"
     
-    visualize_segmented_image(image, image_remove_background, threshold_mask, bounding_boxes, highest_concentration_bounding_boxes, checked_overlapping_boxes)
-    return None
+    segmented_image = apply_mask(image, final_mask)
+    assert segmented_image is not None, "Segmented image is None"
     
-    return segmented_image, bounding_boxes
+    visualize_segmented_image(image, image_remove_background, threshold_mask, bounding_boxes, highest_concentration_bounding_boxes, checked_overlapping_boxes, final_bounding_boxes, final_mask, segmented_image)
+    return segmented_image, final_bounding_boxes
 
-def visualize_segmented_image(image: np.ndarray, image_remove_background: np.ndarray, threshold_mask: np.ndarray, all_boxes: list, highest_boxes: list, checked_boxes: list) -> None:
+def visualize_segmented_image(image: np.ndarray, image_remove_background: np.ndarray, threshold_mask: np.ndarray, all_boxes: list, highest_boxes: list, checked_boxes: list, final_boxes: list, final_mask: np.ndarray, segmented_image: np.ndarray) -> None:
     """
     Visualize the segmented image with detected bounding boxes.
 
@@ -45,6 +50,7 @@ def visualize_segmented_image(image: np.ndarray, image_remove_background: np.nda
     image_all_box = image.copy()
     image_highest_concentration_box = image.copy()
     image_checked_box = image.copy()
+    image_final_box = image.copy()
     for box in all_boxes:
         x, y, w, h = box
         cv2.rectangle(image_all_box, (x, y), (x+w, y+h), (0, 255, 0), 2)
@@ -54,8 +60,11 @@ def visualize_segmented_image(image: np.ndarray, image_remove_background: np.nda
     for box in checked_boxes:
         x, y, w, h = box
         cv2.rectangle(image_checked_box, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        
-    fig, ax = plt.subplots(2, 3, figsize=(18, 18))
+    for box in final_boxes:
+        x, y, w, h = box
+        cv2.rectangle(image_final_box, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
+    fig, ax = plt.subplots(3, 3, figsize=(18, 18))
     ax = ax.ravel()
     ax[0].imshow(image),                           ax[0].set_title("Original Image")
     ax[1].imshow(image_remove_background),         ax[1].set_title("Background Removed")
@@ -63,6 +72,9 @@ def visualize_segmented_image(image: np.ndarray, image_remove_background: np.nda
     ax[3].imshow(image_all_box),                   ax[3].set_title("All Bounding Boxes")
     ax[4].imshow(image_highest_concentration_box), ax[4].set_title("Highest Concentration Bounding Boxes")
     ax[5].imshow(image_checked_box),               ax[5].set_title("Checked Overlapping Boxes")
+    ax[6].imshow(image_final_box),                 ax[6].set_title("Final Bounding Boxes")
+    ax[7].imshow(final_mask, cmap='gray'),         ax[7].set_title("Final Mask")
+    ax[8].imshow(segmented_image),                 ax[8].set_title("Segmented Image")
     for a in ax:
         a.axis("off")
     plt.show()
@@ -103,7 +115,7 @@ def create_threshold_mask(image: np.ndarray) -> np.ndarray:
     kernel_size = max(5, min(image.shape[0] // 100, image.shape[1] // 100))
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel_size, kernel_size))
     threshold = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel, iterations=5)
-    threshold = cv2.morphologyEx(threshold, cv2.MORPH_CLOSE, kernel, iterations=3)
+    threshold = cv2.morphologyEx(threshold, cv2.MORPH_CLOSE, kernel, iterations=5)
     return threshold
 
 def detect_contours(image: np.ndarray):
@@ -285,5 +297,108 @@ def _split_box(image: np.ndarray, box: tuple) -> list:
     else:
         return [box]
     
-    
-    
+def create_bounding_boxes_from_checked_boxes(bounding_boxes: list, checked_boxes: list) -> list:
+    """
+    Create bounding boxes from the checked boxes.
+
+    Args:
+        bounding_boxes (list): List of bounding boxes.
+        checked_boxes (list): List of checked boxes.
+
+    Returns:
+        list: List of final bounding boxes.
+    """
+    final_boxes = []
+    y_coordinates = _get_y_coordinates(checked_boxes)
+    x_coordinates = _get_x_coordinates(bounding_boxes)
+    for y_start, y_end in y_coordinates:
+        for x_start, x_end in x_coordinates:
+            final_boxes.append((x_start, y_start, x_end - x_start, y_end - y_start))
+    return final_boxes
+
+def _get_y_coordinates(boxes: list) -> list:
+    """
+    Get the y-coordinates of the input boxes.
+
+    Args:
+        boxes (list): List of bounding boxes.
+
+    Returns:
+        list: List of y-coordinates.
+    """
+    return [(box[1], box[1] + box[3]) for box in boxes]
+
+def _get_x_coordinates(boxes: list) -> list:
+    """
+    Get the x-coordinates of the input boxes.
+
+    Args:
+        boxes (list): List of bounding boxes.
+
+    Returns:
+        list: List of x-coordinates.
+    """
+    x_coordinates = [(box[0], box[0] + box[2]) for box in boxes]
+    x_coordinates = _merge_intersect_x_coordinates(x_coordinates)
+    return x_coordinates
+
+def _merge_intersect_x_coordinates(x_coordinates: list) -> list:
+    """
+    Merge intersecting x-coordinates.
+
+    Args:
+        x_coordinates (list): List of x-coordinates.
+
+    Returns:
+        list: List of merged x-coordinates.
+    """
+    merged_coordinates = []
+    sorted_x_coordinates = sorted(x_coordinates, key=lambda x: x[0])
+    for x_start, x_end in sorted_x_coordinates:
+        if not merged_coordinates:
+            merged_coordinates.append([x_start, x_end])
+        else:
+            last_start, last_end = merged_coordinates[-1]
+            if x_start > last_end or x_end < last_start:
+                merged_coordinates.append([x_start, x_end])
+            else:
+                merged_coordinates[-1] = [min(x_start, last_start), max(x_end, last_end)]
+    return merged_coordinates
+
+def create_mask_from_bounding_boxes(image_shape: tuple, bounding_boxes: list) -> np.ndarray:
+    """
+    Create a binary mask from the input bounding boxes.
+
+    Args:
+        image_shape (tuple): Shape of the input image.
+        bounding_boxes (list): List of bounding boxes.
+
+    Returns:
+        np.ndarray: Binary mask.
+    """
+    mask = np.zeros(image_shape[:2], dtype=np.uint8)
+    for box in bounding_boxes:
+        x, y, w, h = box
+        mask = cv2.ellipse(img = mask,
+                            center = (x + w // 2, y + h // 2),
+                            axes = (w // 2, h // 2),
+                            angle = 0,
+                            startAngle = 0,
+                            endAngle = 360,
+                            color = 255,
+                            thickness = -1)
+    return mask
+
+def apply_mask(image: np.ndarray, mask: np.ndarray) -> np.ndarray:
+    """
+    Apply a binary mask to the input image.
+
+    Args:
+        image (np.ndarray): Input image.
+        mask (np.ndarray): Binary mask.
+
+    Returns:
+        np.ndarray: Masked image.
+    """
+    assert image.shape[:2] == mask.shape, "Image and mask shape mismatch"
+    return cv2.bitwise_and(image, image, mask=mask)
