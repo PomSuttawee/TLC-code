09e4555eea8c7fd7adf1153c921c267bfd204536
Suttawee Thongkao <112872479+PomSuttawee@users.noreply.github.com>
2025-03-23 18:58:33 +0700

    Refactor mixture processing to enhance bounding box handling and streamline substance data extraction

--- package_2/tlc_object/mixture.py
+++ package_2/tlc_object/mixture.py
@@ -1,64 +1,36 @@
 import numpy as np
 import cv2
 
-from package_2.image_processing.detect_paper import PaperDetector
-from package_2.image_processing.detect_solvent_front_and_origin import TLCImageProcessor
-from package_2.image_processing.segment_substance_spot import segment_hsv_threshold_range
+from package_2.image_processing.crop_paper import PaperDetector
+from package_2.image_processing.crop_solvent_front_and_origin import TLCImageProcessor
+from package_2.image_processing.crop_substance_spot import crop_substance_spot_mixture
 from package_2.data_extractor.mixture_data_extractor import extract_data
 
+# # from package_2.data_extractor.mixture_data_extractor import extract_data
+
 class Substance():
-    def __init__(self, substance_index: int, image: np.ndarray, rf: float, peak_area: float):
-        self.substance_index = substance_index
-        self.image = image
+    def __init__(self, substance_name: str, rf: float, peak_area: float):
+        self.substance_name = substance_name
         self.rf = rf
         self.peak_area = peak_area
+        print(f"Substance {self.substance_name} created.")
+        print(f'Rf: {self.rf}, Peak Area: {self.peak_area}\n')
 
 class MixtureSingleChannel():
-    def __init__(self, name: str, image: np.ndarray):
+    def __init__(self, name: str, image: np.ndarray, bounding_boxes: list):
         self.name = name
         self.image = image
+        self.bounding_boxes = bounding_boxes
         self.substances = self._create_substances()
     
-    def show_substances_data(self):
-        import matplotlib.pyplot as plt
-        from matplotlib.gridspec import GridSpec
-        
-        n_substances = len(self.substances)
-        fig = plt.figure(figsize=(18, 18))
-        
-        # Create custom grid layout
-        gs = GridSpec(n_substances, 3, figure=fig)
-        
-        # Create a subplot that spans all rows in first column
-        ax_main = fig.add_subplot(gs[:, 0])
-        ax_main.imshow(self.image, cmap='gray')
-        ax_main.set_title('Mixture Image')
-        
-        # Create individual subplots for each substance
-        for substance in self.substances.values():
-            i = substance.substance_index
-            
-            # Substance image in second column
-            ax_img = fig.add_subplot(gs[i, 1])
-            ax_img.imshow(substance.image, cmap='gray')
-            
-            # Substance text info in third column
-            ax_text = fig.add_subplot(gs[i, 2])
-            ax_text.axis('off')
-            ax_text.text(0.1, 0.5, f"Substance: {substance.substance_index}\nRF: {substance.rf}\nPeak Area: {substance.peak_area}", 
-                        fontsize=12, ha='left', va='center')
-        
-        plt.tight_layout()
-        plt.show()
-    
     def _extract_data(self):
-        return extract_data(self.image)
+        return extract_data(self.image, self.bounding_boxes)
     
     def _create_substances(self) -> dict:
         substance_data = self._extract_data()
         substances = {}
-        for index, data in substance_data.items():
-            substances[index] = Substance(index, data['spot_image'], data['rf'], data['peak_area'])    
+        for name, data in substance_data.items():
+            substances[name] = Substance(name, data['rf'], data['peak_area'])    
         return substances
     
 class Mixture():
@@ -66,7 +38,15 @@ class Mixture():
         self.name = name
         self.image = image
         self.paper_image = self._process_image()
-        self.segmented_image = self._segment_image()
+        self.segmented_image, self.bounding_boxes = self._segment_image()
+        
+        # import os
+        # path_paper = 'C:\\Users\\Suttawee\\Desktop\\TLC-code\\OUTPUT\\paper_image'
+        # path_segment = 'C:\\Users\\Suttawee\\Desktop\\TLC-code\\OUTPUT\\segment_image'
+        # cv2.imwrite(os.path.join(path_paper, f'{self.name[:-4]}_input.png'), self.paper_image)
+        # cv2.imwrite(os.path.join(path_segment, f'{self.name[:-4]}_mask_algo.png'), self.segmented_image)
+        
+        self.gray_mixture = MixtureSingleChannel(self.name + "_gray", self._convert_to_gray(self.segmented_image), self.bounding_boxes)
         
     def get_images(self):
         return [self.image, self.paper_image, self.segmented_image]
@@ -75,7 +55,7 @@ class Mixture():
         return TLCImageProcessor.crop_solvent_front_and_origin(PaperDetector.crop_to_paper(self.image))
     
     def _segment_image(self):
-        return segment_hsv_threshold_range(self.paper_image)
+        return crop_substance_spot_mixture(self.paper_image)
     
     def _convert_to_gray(self, image: np.ndarray) -> np.ndarray:
         return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
