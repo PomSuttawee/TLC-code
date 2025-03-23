9ba6f9ae563ad36aaf5688b00529ec71ad0f546a
Suttawee Thongkao <112872479+PomSuttawee@users.noreply.github.com>
2025-03-23 22:18:39 +0700

    Refactor image processing imports to use updated package structure and enhance ingredient/mixture initialization

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import logging
-from package.image_processing.general import io
-# from package.tlc_object.ingredient import Ingredient
-# from package.tlc_object.mixture import Mixture
-from package.tlc_object.tlc_analyzer import TLCAnalyzer
-
+from package_2.io import image_io
 from package_2.tlc_object.ingredient import Ingredient
 from package_2.tlc_object.mixture import Mixture
+from package_2.tlc_object.tlc_analyzer import TLCAnalyzer
 
 def initialize_ingredient(ingredient_image_path: str) -> Ingredient:
     if not ingredient_image_path:
         raise ValueError("No ingredient path found.")
     ingredient_name = ingredient_image_path.split('\\')[-1]
-    ingredient_image = io.read_image(ingredient_image_path)
+    ingredient_image = image_io.read_image(ingredient_image_path)
     ingredient_concentration = [5e-2, 8.33e-2, 16.67e-2, 33.33e-2, 50e-2, 66.67e-2, 83.33e-2, 100e-2]
     ingredient = Ingredient(ingredient_name, ingredient_image, ingredient_concentration)
     return ingredient
@@ -19,25 +16,28 @@ def initialize_ingredient(ingredient_image_path: str) -> Ingredient:
 def initailize_mixture(mixture_image_path: str) -> Mixture:   
     if not mixture_image_path:
         raise ValueError("No mixture path found.")
-    mixture_image = io.read_image(mixture_image_path)
+    mixture_image = image_io.read_image(mixture_image_path)
     mixture = Mixture(name = mixture_image_path.split('\\')[-1], image = mixture_image)
     return mixture
 
-def main():
-    SUBSTANCE_NAME = 'LPY'  # [5CY / LPY / NGG]
+def main() -> None:
+    ingredient_image_paths = image_io.load_image_path(input_type="ingredients")
+    for path in ingredient_image_paths:
+        ingredient = initialize_ingredient(path)
+        logging.info(f"Initialized ingredient: {ingredient.name}")
+        # images = ingredient.get_images()
+        # image_io.display_images(images)
     
-    ingredient_image_paths = io.load_image_path(input_type="ingredients", substance_name=SUBSTANCE_NAME)
-    ingredient = initialize_ingredient(ingredient_image_paths[-1])
-    ingredient.visualize_process()
-    
-    mixture_image_paths = io.load_image_path(input_type="mixtures")
-    mixture = initailize_mixture(mixture_image_paths[0])
-    mixture.visualize_process()
+    mixture_image_paths = image_io.load_image_path(input_type="mixtures")
+    for path in mixture_image_paths:
+        mixture = initailize_mixture(path)
+        logging.info(f"Initialized mixture: {mixture.name}")
+        # images = mixture.get_images()
+        # image_io.display_images(images)
 
-    # # TLC Analyzer
+    # TLC Analyzer
     # tlc_analyzer = TLCAnalyzer(mixture, ingredient)
-    # red_percentage = tlc_analyzer.red_analyzer.equation_solver.show_process()
-    # logging.info("TLC analysis completed.")
+    logging.info("TLC analysis completed.")
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(name)s -> %(funcName)s: %(message)s', level=logging.INFO)
