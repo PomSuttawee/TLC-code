Refactor peak area calculation to correct average value computation in ingredient and mixture data extractors

package/data_extractor/ingredient_data_extractor.py
@@ -34,8 +34,6 @@ def _get_right_most_boxes(bounding_boxes: list) -> list:
     """
     return [boxes[-1] for boxes in bounding_boxes]
 
-
-
 def calculate_calibration_curve(image: np.ndarray, bounding_boxes: list, concentration_list: list) -> list:
     """
     Calculate the calibration curve of the spots in the image.
@@ -61,7 +59,7 @@ def _calculate_peak_areas(image: np.ndarray, horizontal_boxes: list) -> list:
         sum_value = np.sum(spot_image, axis=0)
         count_color_pixel = np.sum(np.where(spot_image > 0, 1, 0), axis=0)
         safe_count_color_pixel = np.where(count_color_pixel == 0, 1, count_color_pixel)
-        average_value = (255 - (sum_value / safe_count_color_pixel)).astype(int)
+        average_value = (sum_value / safe_count_color_pixel).astype(int)
         average_value[count_color_pixel == 0] = 0
         
         peak_area = np.trapz(average_value)

package/data_extractor/mixture_data_extractor.py
@@ -29,7 +29,7 @@ def calculate_peak_area(image: np.ndarray, bounding_boxes: list) -> list:
         sum_value = np.sum(spot_image, axis=0)
         count_color_pixel = np.sum(np.where(spot_image > 0, 1, 0), axis=0)
         safe_count_color_pixel = np.where(count_color_pixel == 0, 1, count_color_pixel)
-        average_value = (255 - (sum_value / safe_count_color_pixel)).astype(int)
+        average_value = (sum_value / safe_count_color_pixel).astype(int)
         average_value[count_color_pixel == 0] = 0
         
         peak_area = np.trapz(average_value)