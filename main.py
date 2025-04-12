4cdf3a14c22c8f14b2e80e43ce76d8b2c1bd97ec
Suttawee Thongkao <112872479+PomSuttawee@users.noreply.github.com>
2025-04-12 15:33:44 +0700

    Refactor ingredient concentration values and add run_all_samples function for batch processing of mixtures

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def initialize_ingredient(ingredient_image_path: str) -> Ingredient:
         raise ValueError("No ingredient path found.")
     ingredient_name = ingredient_image_path.split('\\')[-1]
     ingredient_image = image_io.read_image(ingredient_image_path)
-    ingredient_concentration = [5e-2, 8.33e-2, 16.67e-2, 33.33e-2, 50e-2, 66.67e-2, 83.33e-2, 100e-2]
+    ingredient_concentration = [5, 8.33, 16.67, 33.33, 50, 66.67, 83.33, 100]
     ingredient = Ingredient(ingredient_name, ingredient_image, ingredient_concentration)
     return ingredient
 
@@ -36,6 +36,46 @@ def test_mixture() -> None:
         images = mixture.get_images()
         image_io.display_images(images)
 
+def run_all_samples() -> None:
+    ingredient_image_paths = image_io.load_image_path(input_type="ingredients")
+    mixture_image_paths = image_io.load_image_path(input_type="mixtures")
+    
+    ing_dict = {
+        '5CY': 0,
+        'LPY': 2,
+        'NGG': 6
+    }
+    
+    mixture_ing_dict = {
+        0: ['5CY', 'LPY', 'NGG'],
+        1: ['5CY', 'LPY'],
+        2: ['5CY', 'NGG'],
+        3: ['LPY', 'NGG'],
+        4: ['5CY', 'LPY'],
+        5: ['5CY', 'NGG'],
+        6: ['LPY', 'NGG'],
+        7: ['5CY', 'LPY', 'NGG'],
+        8: ['5CY', 'LPY', 'NGG'], 
+    }
+    
+    for mixture_index, ingredient_indices in mixture_ing_dict.items():
+        print(f"Mixture {mixture_index+1} with ingredients {ingredient_indices}")
+        mixture = initailize_mixture(mixture_image_paths[mixture_index])
+        ingredients = [initialize_ingredient(ingredient_image_paths[ing_dict[i]]) for i in ingredient_indices]
+        
+        tlc_analyzer = TLCAnalyzer(mixture, ingredients)
+        tlc_analyzer.print_result()
+    
+    for mixture_index, ingredient_indices in mixture_ing_dict.items():
+        new_mixture_index = 9 + mixture_index
+        print(f"Mixture {new_mixture_index+1} with ingredients {ingredient_indices}")
+        mixture = initailize_mixture(mixture_image_paths[new_mixture_index])
+        ingredients = [initialize_ingredient(ingredient_image_paths[ing_dict[i]]) for i in ingredient_indices]
+        
+        tlc_analyzer = TLCAnalyzer(mixture, ingredients)
+        tlc_analyzer.print_result()
+    
+
 def main() -> None:
     # try:
     #     test_ingredient()
@@ -44,25 +84,25 @@ def main() -> None:
     #     logging.error(e)
     #     raise e
     
-    
     ingredient_image_paths = image_io.load_image_path(input_type="ingredients")
     mixture_image_paths = image_io.load_image_path(input_type="mixtures")
     
-    MIXTURE_NUM = 0
-    INGREDIENT_NUM = 2
-    ingredients = [initialize_ingredient(ingredient_image_paths[i]) for i in range(INGREDIENT_NUM)]
-    mixture = initailize_mixture(mixture_image_paths[MIXTURE_NUM])
+    MIXTURE_INDEX = 0
+    mixture = initailize_mixture(mixture_image_paths[MIXTURE_INDEX])
     
-    logging.info("TLC analysis started.")
-    tlc_analyzer = TLCAnalyzer(mixture, ingredients)
-    tlc_analyzer.show_data()
-    logging.info("TLC analysis completed.")
+    selected_path = [ingredient_image_paths[2]]
+    ingredients = [initialize_ingredient(path) for path in selected_path]
     
-    ingredient_image = ingredients[0].get_images()[-1] + ingredients[1].get_images()[-1]
-    mixture_image = mixture.get_images()
+    # logging.info("TLC analysis started.")
+    # tlc_analyzer = TLCAnalyzer(mixture, ingredients)
+    # tlc_analyzer.print_result()
+    # logging.info("TLC analysis completed.")
+    
+    ingredient_image = [ingredients[i].get_images()[-1] for i in range(len(ingredients))]
+    mixture_image = [mixture.get_images()[-1]]
     image_io.display_images(ingredient_image + mixture_image)
 
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(name)s -> %(funcName)s: %(message)s', level=logging.INFO)
+    # logging.basicConfig(format='%(name)s -> %(funcName)s: %(message)s', level=logging.INFO)
     main()
